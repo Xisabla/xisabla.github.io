4f28f893d2bb450d31d045fa08dec91389d5145a
Gautier Miquet <xisabla.dev@gmail.com>
2022-02-03 16:49:29 +0100

    rework color palette and harmonize css with vars, also add stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	rules: {
+		'no-descending-specificity': null,
+	},
+	extends: [
+		'stylelint-config-standard',
+		'stylelint-config-prettier',
+		'stylelint-config-recess-order',
+	],
+}
